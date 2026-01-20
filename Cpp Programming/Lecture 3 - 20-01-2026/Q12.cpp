0b31cf2250137f35a4d229d46b15b6c80ba24e4d
jayeshkushwaha <anshkushwaha072024@gmail.com>
2026-01-20 20:47:43 +0530

    Learning c++

---
+++ Cpp Programming/Lecture 3 - 20-01-2026/Q12.cpp
@@ -0,0 +1,32 @@
+/*
+Q 12:
+n=5
+        *
+      * ! *
+    * ! * ! *
+  * ! * ! * ! *
+* ! * ! * ! * ! *
+
+
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    for(int i = 1; i <= n; i++) {
+        for(int s = 1; s <= n - i; s++) {
+            cout << "  ";
+        }
+        for(int j = 1; j <= 2*i - 1; j++) {
+            if(j % 2 == 1)
+                cout << "* ";
+            else
+                cout << "! ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
